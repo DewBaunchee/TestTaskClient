a4b66f74d0b83e4b66cdb8bb28bcd87ecc728c1c
DewBaunchee <vmegabit@gmail.com>
2021-07-09 18:16:32 +0300

    first commit

---
+++ src/app/entities/sensor.ts
@@ -0,0 +1,11 @@
+export interface Sensor {
+    id: number;
+    name: string;
+    model: string;
+    type: string;
+    rangeFrom: number;
+    rangeTo: number;
+    unit: string;
+    location: string,
+    description:string
+}
